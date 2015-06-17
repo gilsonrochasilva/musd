95b3b9d661ba5ddcc10ec90dae8ad3369d85877d
Domingos Santos <domsantos@gmail.com>
2015-06-17 17:55:05 -0300

    GUI do Sincronizador - com ação - parcial

--- musd-sincronizador/src/main/java/br/com/musd/sync/syncGUI.java
+++ musd-sincronizador/src/main/java/br/com/musd/sync/syncGUI.java
@@ -1,16 +1,16 @@
 package br.com.musd.sync;
 
 import javax.swing.*;
-import java.util.Timer;
+import java.net.Socket;
 
 /**
  * Created by domingos on 17/06/15.
  */
 public class syncGUI extends JFrame{
 
-    Timer timer;
-
     private JTextArea txInfo = new JTextArea();
+    private JButton btControle = new JButton();
+    private JButton btFechar = new JButton();
 
     public syncGUI(){
         super("Módulo Sincronizador MUSD");
@@ -19,12 +19,20 @@ public class syncGUI extends JFrame{
 
     private void componentesGUI(){
         this.getContentPane().add(txInfo);
+
     }
 
     public static void main(String[] args){
         syncGUI janela = new syncGUI();
         janela.setSize(640,480);
         janela.setVisible(true);
+        janela.txInfo.setText("aqui");
+
+
+        Socket socket;
+
     }
+
+
 
 }
