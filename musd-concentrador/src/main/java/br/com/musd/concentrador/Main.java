817b95ee9e2694c93be77b0c195edbe8313c0e6b
Gilson Rocha Silva <gilson.silva@cinbesa.pmb.pa.gov.br>
2015-06-17 17:51:43 -0300

    Configuraçao Maven: inclusao da dependencia ao musd-model nos outros projetos;

---
+++ musd-concentrador/src/main/java/br/com/musd/concentrador/Main.java
@@ -0,0 +1,24 @@
+package br.com.musd.concentrador;
+
+/**
+ * Created by gilson on 17/06/15.
+ */
+public class Main {
+
+
+
+    public Main() {
+    }
+
+    /**
+     * Carrega os objetos
+     *
+     */
+    private void carregarDados() {
+
+    }
+
+    public static void main(String args[]) {
+        new Main();
+    }
+}
