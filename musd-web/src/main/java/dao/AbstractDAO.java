d605016974774e9e0bec09833d688fd205779f8d
Stélio Corrêa <stelio.correa@gmail.com>
2015-06-17 19:52:17 -0300

    Commit EASoftware :)

--- musd-web/src/main/java/dao/AbstractDAO.java
+++ musd-web/src/main/java/dao/AbstractDAO.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 public abstract class AbstractDAO<T> implements DAO<T> {
 
-    @Override
     public abstract void create(T object);
 
-    @Override
     public abstract void update(T object);
 
-    @Override
     public abstract void remove(T object);
 
-    @Override
     public abstract List<T> findAll();
 }
