4d69016b1ca31a1ed29f279787f847ccbf9d55e1
Gilson Rocha Silva <gilson.silva@cinbesa.pmb.pa.gov.br>
2015-06-23 20:21:36 -0300

    Classes DAO;

---
+++ musd-web/src/main/java/dao/BandoDadosDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+import br.com.musd.administrativo.BancoDados;
+import dao.common.DAO;
+
+/**
+ * Created by gilson on 23/06/15.
+ */
+public class BandoDadosDAO extends DAO<BancoDados> {
+}
