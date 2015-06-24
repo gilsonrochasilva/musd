ff2045415ae42d1ab4df5f3fb37353ce10b14b12
marcos <marcos.chagas@cinbesa.pmb.pa.gov.br>
2015-06-24 20:56:44 -0300

    Formulario de Model (parcial)

--- musd-web/src/main/java/br/com/musd/mb/ModeloFormBean.java
+++ musd-web/src/main/java/br/com/musd/mb/ModeloFormBean.java
@@ -1,13 +1,19 @@
 package br.com.musd.mb;
 
+import br.com.musd.administrativo.Master;
 import br.com.musd.administrativo.ModeloMigracao;
+import br.com.musd.administrativo.Slave;
 import br.com.musd.mb.common.PaginaBean;
+import br.com.musd.service.ModeloMigracaoSrv;
 import com.ocpsoft.pretty.faces.annotation.URLAction;
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by correa on 24/06/15.
@@ -21,9 +27,22 @@ public class ModeloFormBean extends PaginaBean {
 
     private ModeloMigracao modeloMigracao;
 
+    private Master master;
+
+    private List<Slave> slaves;
+
+    @EJB private ModeloMigracaoSrv modeloMigracaoSrv;
+
     @URLAction(mappingId = "novoModelo")
     public void init() {
         modeloMigracao = new ModeloMigracao();
+        master = new Master();
+        slaves = new ArrayList<Slave>();
+    }
+
+    public void salvar() {
+        modeloMigracaoSrv.salvar(modeloMigracao, master, slaves);
+        addInfo("Modelo salvo com sucesso!");
     }
 
     public ModeloMigracao getModeloMigracao() {
@@ -32,5 +51,21 @@ public class ModeloFormBean extends PaginaBean {
 
     public void setModeloMigracao(ModeloMigracao modeloMigracao) {
         this.modeloMigracao = modeloMigracao;
+    }
+
+    public Master getMaster() {
+        return master;
+    }
+
+    public void setMaster(Master master) {
+        this.master = master;
+    }
+
+    public List<Slave> getSlaves() {
+        return slaves;
+    }
+
+    public void setSlaves(List<Slave> slaves) {
+        this.slaves = slaves;
     }
 }
--- musd-web/src/main/java/br/com/musd/service/ModeloMigracaoSrv.java
+++ musd-web/src/main/java/br/com/musd/service/ModeloMigracaoSrv.java
@@ -1,9 +1,15 @@
 package br.com.musd.service;
 
+import br.com.musd.administrativo.Master;
 import br.com.musd.administrativo.ModeloMigracao;
+import br.com.musd.administrativo.Slave;
+import br.com.musd.dao.MasterDAO;
 import br.com.musd.dao.ModeloMigracaoDAO;
+import br.com.musd.dao.SlaveDAO;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import java.util.List;
 
@@ -17,6 +23,12 @@ public class ModeloMigracaoSrv {
     @Inject
     private ModeloMigracaoDAO modeloMigracaoDAO;
 
+    @Inject
+    private MasterDAO masterDAO;
+
+    @Inject
+    private SlaveDAO slaveDAO;
+
     public ModeloMigracao getUm(Integer id) {
         return modeloMigracaoDAO.getUm(id, ModeloMigracao.class);
     }
@@ -25,14 +37,28 @@ public class ModeloMigracaoSrv {
         return modeloMigracaoDAO.listar(ModeloMigracao.class);
     }
 
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void atualizar(ModeloMigracao modeloMigracao)
     {
         modeloMigracaoDAO.atualizar(modeloMigracao);
     }
 
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public void salvar(ModeloMigracao modeloMigracao)
     {
         modeloMigracaoDAO.salvar(modeloMigracao);
     }
 
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void salvar(ModeloMigracao modeloMigracao, Master master, List<Slave> slaves) {
+        salvar(modeloMigracao);
+
+        master.setModeloMigracao(modeloMigracao);
+        masterDAO.salvar(master);
+
+        for (Slave slave : slaves) {
+            slave.setModeloMigracao(modeloMigracao);
+            slaveDAO.salvar(slave);
+        }
+    }
 }
