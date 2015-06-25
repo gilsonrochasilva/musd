fec1efdbfdc7f6e6849440c175b739c707ad8425
Domingos Santos <domsantos@gmail.com>
2015-06-25 20:41:13 -0300

    Lista de Slave Parcial

--- musd-web/src/main/java/br/com/musd/dao/SlaveDAO.java
+++ musd-web/src/main/java/br/com/musd/dao/SlaveDAO.java
@@ -1,10 +1,22 @@
 package br.com.musd.dao;
 
+import br.com.musd.administrativo.ModeloMigracao;
 import br.com.musd.administrativo.Slave;
 import br.com.musd.dao.common.DAO;
+
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Created by gilson on 23/06/15.
  */
 public class SlaveDAO extends DAO<Slave> {
+
+    public List<Slave> listaPorModelo(ModeloMigracao modeloMigracao){
+        Query query = getEm().createQuery("select s from Slave s where s.modeloMigracao = :modelo");
+        query.setParameter("modelo",modeloMigracao);
+
+        return query.getResultList();
+
+    }
 }
